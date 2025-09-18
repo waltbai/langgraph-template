944bfb45610685fe2d045a04aeb09e310bae22dc
waltbai <bailong@ict.ac.cn>
2025-09-18 17:29:16 +0800

    [feat] add cli example

---
+++ src/example/cli.py
@@ -0,0 +1,23 @@
+"""Command line interface."""
+
+import json
+
+from dotenv import load_dotenv
+
+from example.graph import graph
+
+
+def main():
+    """Main."""
+    load_dotenv()
+    agent = graph().compile()
+    for chunk in agent.stream(
+        input={},
+        context={},
+        stream_mode="updates",
+    ):
+        print(chunk)
+
+
+if __name__ == "__main__":
+    main()
